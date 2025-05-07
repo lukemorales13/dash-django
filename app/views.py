3b119f725e03db086eea2883ddf8d0efea033d51
Yael Lozano <lozanoy6@gmail.com>
2025-05-06 21:05:42 -0600

    luk peyejo

--- app/views.py
+++ app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+import plotly.express as px
+import plotly.graph_objs as go
+import plotly.io as pio
+import random 
 
 # Create your views here.
 
+VARIABLES = ['A', 'B', 'C', 'D']
 def main_dashboard(request):
-    context = {}
-    return render(request, 'content.html', context)
+    regions = request.GET.getlist('region')
+    
+    if not regions:
+        regions = TODAS_LAS_REGIONES
+
+    y = [random.randint(5, 20) for _ in regions]
+    fig = go.Figure(data=go.Bar(x=regions, y=y, name="Ventas"))
+    chart_html = pio.to_html(fig, include_plotlyjs='cdn')
+
+    return render(request, 'content.html', {
+        'plot': chart_html,
+        'regions': regions,
+        'all': VARIABLES,
+    })
